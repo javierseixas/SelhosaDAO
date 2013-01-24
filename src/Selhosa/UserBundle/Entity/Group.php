68d1975db3bbff06d1aa20e3066cbd136cbcd39f
Javier Seixas <contacte@javierseixas.com>
2013-01-24 07:43:06 +0100

    User and Group entities using annotations

---
+++ src/Selhosa/UserBundle/Entity/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Selhosa\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Groups")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
--- src/Selhosa/UserBundle/Entity/User.php
+++ src/Selhosa/UserBundle/Entity/User.php
@@ -4,6 +4,8 @@ namespace Selhosa\UserBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Selhosa\ReparationBundle\Entity\WorkOrder;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
 * @ORM\Entity
@@ -20,7 +22,49 @@ class User extends BaseUser
 
     public function __construct()
     {
-    parent::__construct();
-    // your own logic
+        parent::__construct();
+
+        $this->workorders = new ArrayCollection();
+    }
+
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Selhosa\UserBundle\Entity\Group")
+     * @ORM\JoinTable(name="User_Groups",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
+    /**
+     * @ManyToMany(targetEntity="WorkOrder", inversedBy="technicians")
+     * @JoinTable(name="Technician_WorkOrder")
+     */
+    protected $workorders;
+
+    /**
+     * @param \Selhosa\ReparationBundle\Entity\WorkOrder $workorder
+     */
+    public function addWorkorders(WorkOrder $workorder)
+    {
+        if (!$this->workorders->contains($workorder))
+            $this->workorders->add($workorder);
+    }
+
+    /**
+     * @param \Selhosa\ReparationBundle\Entity\WorkOrder $workorder
+     */
+    public function removeWorkorder(WorkOrder $workorder)
+    {
+        $this->clients->removeElement($workorder);
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getWorkorders()
+    {
+        return $this->workorders;
     }
 }
