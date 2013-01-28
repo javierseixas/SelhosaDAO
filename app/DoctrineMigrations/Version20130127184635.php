5db77c77e870ddef8d3af38ab8182620bf611ee0
Javier Seixas <contacte@javierseixas.com>
2013-01-28 08:00:24 +0100

    Add index to WorkOrder reference field

---
+++ app/DoctrineMigrations/Version20130127184635.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130127184635 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("CREATE INDEX reference_IDX_1 ON WorkOrder (reference)");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("DROP INDEX reference_IDX_1 ON WorkOrder");
+    }
+}
