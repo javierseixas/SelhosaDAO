2beb0764e8ef4f504b339278a112a73016c74830
Javier Seixas <contacte@javierseixas.com>
2013-02-04 15:34:53 +0100

    Start adding ElectronicBundle (non stable)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Selhosa\UserBundle\UserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Selhosa\ElectronicBundle\SelhosaElectronicBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Selhosa/ElectronicBundle/SelhosaElectronicBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Selhosa\ElectronicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SelhosaElectronicBundle extends Bundle
+{
+}
